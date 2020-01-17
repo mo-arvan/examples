8e901b56d9d02ffe382063268b6f72b23bb03024
Arvan <mo.arvan@gmail.com>
2020-01-16 18:56:23 -0600

    Update code and unit tests

--- word_language_model/lstm.py
+++ word_language_model/lstm.py
@@ -1,8 +1,7 @@
-from typing import Any
+from typing import Tuple, List, Optional
 
 import torch
 import torch.nn as nn
-from typing import Tuple, List, Optional
 
 """
 Implementation of LSTM model described in On the State of the Art of Evaluation in Neural Language Models
@@ -11,153 +10,95 @@ https://arxiv.org/abs/1707.05589
 
 
 class LSTM(nn.Module):
-  """
-  The LSTM expects the input to be batch first: Batch size x length
-  The LSTM does not support bidirectional as an argument
-  """
-  __constants__ = ['layers']
-
-  def __init__(self, input_size, hidden_size,
-               num_layers=1, bias=True,
-               inter_layer_dropout=0.1, recurrent_dropout=0.,
-               skip_connection=False):
-    super(LSTM, self).__init__()
-
-    assert num_layers >= 1 and inter_layer_dropout >= 0. and recurrent_dropout >= 0.
-
-    self.layers = nn.ModuleList([
-      nn.LSTMCell(input_size=input_size, hidden_size=hidden_size, bias=bias)
-    ])
-    for layer in range(num_layers - 1):
-      self.layers.append(nn.LSTMCell(input_size=hidden_size, hidden_size=hidden_size, bias=bias))
-
-    self.inter_layer_dropout = nn.Dropout(p=inter_layer_dropout)
-    self.recurrent_dropout = nn.Dropout(p=recurrent_dropout)
-
-    self.num_layers = num_layers
-    self.hidden_size = hidden_size
-    self.skip_connection = skip_connection
-
-  def forward(self, input: torch.Tensor, hx: Optional[Tuple[torch.Tensor, torch.Tensor]] = None) -> Tuple[
-    torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
     """
-
-    :param input: batch size x length x input_size
-    :param hx: num_layers x batch_size x hidden_size
-    :return: batch size x length x input_size
+    The LSTM expects the input to be batch first: Batch size x length
+    The LSTM does not support bidirectional as an argument
     """
-    input_length = input.shape[1]
-    batch_size = input.shape[0]
+    __constants__ = ['layers']
 
-    if hx is None:
-      zeros = torch.zeros(self.num_layers,
-                          batch_size, self.hidden_size,
-                          dtype=input.dtype, device=input.device)
-      hx = (zeros, zeros)
+    def __init__(self, input_size, hidden_size,
+                 num_layers=1, bias=True,
+                 inter_layer_dropout=0.1, recurrent_dropout=0.,
+                 skip_connection=False, batch_first=False):
+        super(LSTM, self).__init__()
 
-    # time x num_layers x batch_size x hidden_state
-    # lstm_h_list, lstm_c_list = input.new_full(size=(input_length + 1, self.num_layers,
-    #                                                 batch_size, self.hidden_size), fill_value=0.), \
-    #                            input.new_full(size=(input_length + 1, self.num_layers,
-    #                                                 batch_size, self.hidden_size), fill_value=0.)
-    length_dim_tensor = hx[0][0]
-    lstm_h_list: List[List[torch.Tensor]] = [[torch.zeros_like(length_dim_tensor)] * self.num_layers] * (
-        input_length + 1)
-    lstm_c_list: List[List[torch.Tensor]] = [[torch.zeros_like(length_dim_tensor)] * self.num_layers] * (
-        input_length + 1)
+        assert num_layers >= 1 and inter_layer_dropout >= 0. and recurrent_dropout >= 0.
 
-    # lstm_h_list[0] = hx[0]
-    # lstm_c_list[0] = hx[1]
+        self.layers = nn.ModuleList([
+            nn.LSTMCell(input_size=input_size, hidden_size=hidden_size, bias=bias)
+        ])
+        for layer in range(num_layers - 1):
+            self.layers.append(nn.LSTMCell(input_size=hidden_size, hidden_size=hidden_size, bias=bias))
 
-    lstm_h_list[0] = hx[0].unbind(dim=0)
-    lstm_c_list[0] = hx[1].unbind(dim=0)
+        self.inter_layer_dropout = nn.Dropout(p=inter_layer_dropout)
+        self.recurrent_dropout = nn.Dropout(p=recurrent_dropout)
 
-    layer_index = -1
-    for layer in self.layers:
-      layer_index += 1
-      for index in range(input.shape[1]):
-        if layer_index == 0:
-          lstm_input = input.select(dim=1, index=index)
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.skip_connection = skip_connection
+        self.batch_first = batch_first
+
+    def forward(self, input: torch.Tensor, hx: Optional[Tuple[torch.Tensor, torch.Tensor]] = None) -> Tuple[
+        torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        """
+
+        :param input: batch size x length x input_size
+        :param hx: num_layers x batch_size x hidden_size
+        :return: batch size x length x input_size
+        """
+        if not self.batch_first:
+            input = input.permute(1, 0, 2)
+        input_length = input.shape[1]
+        batch_size = input.shape[0]
+
+        if hx is None:
+            zeros = torch.zeros(self.num_layers,
+                                batch_size, self.hidden_size,
+                                dtype=input.dtype, device=input.device)
+            hx = (zeros, zeros)
+
+        length_dim_tensor = hx[0][0]
+
+        lstm_h_list: List[List[torch.Tensor]] = []
+        lstm_c_list: List[List[torch.Tensor]] = []
+
+        for i in range(input_length + 1):
+            lstm_h_list.append([])
+            lstm_c_list.append([])
+            for __ in range(self.num_layers):
+                lstm_h_list[i].append(torch.zeros_like(length_dim_tensor))
+                lstm_c_list[i].append(torch.zeros_like(length_dim_tensor))
+
+        lstm_h_list[0][0] = torch.ones_like(lstm_h_list[0][0])
+
+        lstm_h_list[0] = hx[0].unbind(dim=0)
+        lstm_c_list[0] = hx[1].unbind(dim=0)
+
+        layer_index = -1
+        for layer in self.layers:
+            layer_index += 1
+            for index in range(input_length):
+                if layer_index == 0:
+                    lstm_input = input.select(dim=1, index=index)
+                else:
+                    lstm_input = lstm_h_list[index + 1][layer_index - 1]
+                    lstm_input = self.inter_layer_dropout(lstm_input)
+                h_0, c_0 = lstm_h_list[index][layer_index], lstm_c_list[index][layer_index]
+                h_0, c_0 = self.recurrent_dropout(h_0), self.recurrent_dropout(c_0)
+                h_1, c_1 = layer(lstm_input, (h_0, c_0))
+                lstm_h_list[index + 1][layer_index] = h_1.clone()
+                lstm_c_list[index + 1][layer_index] = c_1.clone()
+
+        lstm_h = torch.stack([torch.stack(l) for l in lstm_h_list])
+        lstm_c = torch.stack([torch.stack(l) for l in lstm_c_list])
+
+        output = lstm_h[1:]
+
+        if self.skip_connection:
+            output = output.sum(dim=1)
         else:
-          lstm_input = lstm_h_list[index + 1][layer_index - 1]
-          lstm_input = self.inter_layer_dropout(lstm_input)
-        h_0, c_0 = lstm_h_list[index][layer_index], lstm_c_list[index][layer_index]
-        h_0, c_0 = self.recurrent_dropout(h_0), self.recurrent_dropout(c_0)
-        h_1, c_1 = layer(lstm_input, (h_0, c_0))
-        lstm_h_list[index + 1][layer_index] = h_1
-        lstm_c_list[index + 1][layer_index] = c_1
+            output = output[:, -1, :, :]
 
-    lstm_h_list = torch.stack([torch.stack(l) for l in lstm_h_list])
-    lstm_c_list = torch.stack([torch.stack(l) for l in lstm_c_list])
-
-    output = lstm_h_list[1:]
-
-    if self.skip_connection:
-      output = output.sum(dim=1)
-    else:
-      output = output[:, -1, :, :]
-
-    output = output.permute(1, 0, 2)
-    return output, (lstm_h_list[-1], lstm_c_list[-1])
-
-
-class RNNLM(nn.Module):
-  """
-  RNN LM input dropout, intra-layer dropout, output dropout, recurrent states
-  # rnn_type, ntoken, ninp, nhid, nlayers, dropout = 0.5, tie_weights = False
-  # input_size, hidden_size,
-  # num_layers = 1, bias = True,
-  # inter_layer_dropout = 0., recurrent_dropout = 0.
-  """
-
-  def __init__(self, num_tokens, input_size, hidden_size,
-               num_layers=1, bias=True,
-               inter_layer_dropout=0., recurrent_dropout=0.,
-               input_dropout=0., output_dropout=0.,
-               tie_weights=False):
-    super(RNNLM, self).__init__()
-
-    self.embedding = nn.Embedding(num_embeddings=num_tokens, embedding_dim=input_size)
-
-    self.lstm = LSTM(input_size=input_size, hidden_size=hidden_size,
-                     num_layers=num_layers, bias=bias,
-                     inter_layer_dropout=inter_layer_dropout, recurrent_dropout=recurrent_dropout)
-
-    self.input_dropout = nn.Dropout(p=input_dropout)
-    self.output_dropout = nn.Dropout(p=output_dropout)
-
-    self.decoder = nn.Linear(hidden_size, num_tokens)
-
-    if tie_weights:
-      if input_size != hidden_size:
-        raise ValueError('When using the tied flag, nhid must be equal to emsize')
-      self.decoder.weight = self.embedding.weight
-
-    self.num_layers = num_layers
-    self.hidden_size = hidden_size
-
-  def init_weights(self):
-    initrange = 0.1
-    self.embedding.weight.data.uniform_(-initrange, initrange)
-    self.decoder.bias.data.zero_()
-    self.decoder.weight.data.uniform_(-initrange, initrange)
-
-  def forward(self, input: torch.Tensor, hidden: Optional[Tuple[torch.Tensor, torch.Tensor]] = None):
-    """
-
-    :param input: batch size x length
-    :param hidden: num_layers x batch_size x hidden_size
-    :return:
-    """
-    embedded = self.input_dropout(self.embedding(input))
-    # making the input batch first
-    embedded = embedded.permute(1, 0, 2)
-    output, hidden = self.lstm(embedded, hidden)
-    output = self.output_dropout(output)
-    decoded = self.decoder(output)
-    return decoded, hidden
-
-  def init_hidden(self, bsz):
-    weight = next(self.parameters())
-    return (weight.new_zeros(self.num_layers, bsz, self.hidden_size),
-            weight.new_zeros(self.num_layers, bsz, self.hidden_size))
+        if self.batch_first:
+            output = output.permute(1, 0, 2)
+        return output, (lstm_h[-1], lstm_c[-1])
--- word_language_model/lstm_test.py
+++ word_language_model/lstm_test.py
@@ -1,177 +1,183 @@
 import unittest
+
 import torch
-import torch.nn
-import lstm
-import torch.testing as torch_testing
-import random
-import numpy as np
 import torch.jit
+import torch.nn
+import torch.testing as torch_testing
+
+import lstm
 
 
 class CustomLSTMTest(unittest.TestCase):
 
-  def test_lstm_basic_single_layer(self):
-    self.set_seed()
-    # :param input: batch size x length x input_size
-    # :param hx: num_layers x batch_size x hidden_size
+    def test_lstm_basic_single_layer(self):
+        self.set_seed()
+        # :param input: batch size x length x input_size
+        # :param hx: num_layers x batch_size x hidden_size
 
-    batch_size = 13
-    length = 4
-    input_size = 32
-    hidden_size = 64
-    num_layers = 1
-    bias = True
-    inter_layer_dropout = 0.
-    recurrent_dropout = 0.
-    input = torch.rand(batch_size, length, input_size)
-    hx = torch.rand(num_layers, batch_size, hidden_size), torch.rand(num_layers, batch_size, hidden_size)
+        batch_size = 13
+        length = 2
+        input_size = 32
+        hidden_size = 64
+        num_layers = 1
+        bias = True
+        inter_layer_dropout = 0.
+        recurrent_dropout = 0.
+        batch_first = True
+        skip_connection = False
+        jit_forward_custom = False
 
-    torch_lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                               num_layers=num_layers, bias=bias, dropout=inter_layer_dropout,
-                               batch_first=True, bidirectional=False)
+        self.compare_custom_and_cuda(batch_size, length, input_size, hidden_size, num_layers, bias,
+                                     inter_layer_dropout, recurrent_dropout, batch_first, skip_connection,
+                                     jit_forward_custom)
 
-    custom_lstm = lstm.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, bias=bias,
-                            inter_layer_dropout=inter_layer_dropout, recurrent_dropout=recurrent_dropout)
+    def test_lstm_basic_multi_layer(self):
+        self.set_seed()
+        # :param input: batch size x length x input_size
+        # :param hx: num_layers x batch_size x hidden_size
 
-    self.copy_weights_from_torch_to_custom(torch_lstm, custom_lstm)
+        batch_size = 13
+        length = 7
+        input_size = 32
+        hidden_size = 64
+        num_layers = 4
+        bias = True
+        inter_layer_dropout = 0.
+        recurrent_dropout = 0.
+        skip_connection = False
+        batch_first = True
+        jit_forward_custom = False
 
-    torch_output, torch_hidden = torch_lstm(input, hx)
-    custom_output, custom_hidden = custom_lstm(input, hx)
+        self.compare_custom_and_cuda(batch_size, length, input_size, hidden_size, num_layers, bias,
+                                     inter_layer_dropout, recurrent_dropout, batch_first, skip_connection,
+                                     jit_forward_custom)
 
-    torch_testing.assert_allclose(torch_hidden[0], custom_hidden[0])
-    torch_testing.assert_allclose(torch_hidden[1], custom_hidden[1])
+    def test_lstm_basic_multi_layer_length_first(self):
+        self.set_seed()
+        # :param input: batch size x length x input_size
+        # :param hx: num_layers x batch_size x hidden_size
 
-    torch_testing.assert_allclose(custom_output, torch_output)
+        batch_size = 13
+        length = 7
+        input_size = 32
+        hidden_size = 64
+        num_layers = 4
+        bias = True
+        inter_layer_dropout = 0.
+        recurrent_dropout = 0.
+        skip_connection = False
+        batch_first = False
+        jit_forward_custom = False
 
-  def test_lstm_basic_multi_layer(self):
-    self.set_seed()
-    # :param input: batch size x length x input_size
-    # :param hx: num_layers x batch_size x hidden_size
+        self.compare_custom_and_cuda(batch_size, length, input_size, hidden_size, num_layers, bias,
+                                     inter_layer_dropout, recurrent_dropout, batch_first, skip_connection,
+                                     jit_forward_custom)
 
-    batch_size = 13
-    length = 7
-    input_size = 32
-    hidden_size = 64
-    num_layers = 4
-    bias = True
-    inter_layer_dropout = 0.
-    recurrent_dropout = 0.
-    skip_connection = False
+    def test_lstm_basic_multi_layer_without_bias(self):
+        self.set_seed()
+        # :param input: batch size x length x input_size
+        # :param hx: num_layers x batch_size x hidden_size
 
-    input = torch.rand(batch_size, length, input_size)
-    hx = torch.rand(num_layers, batch_size, hidden_size), torch.rand(num_layers, batch_size, hidden_size)
+        batch_size = 13
+        length = 7
+        input_size = 32
+        hidden_size = 64
+        num_layers = 4
+        bias = False
+        inter_layer_dropout = 0.
+        recurrent_dropout = 0.
+        batch_first = True
+        skip_connection = False
+        jit_forward_custom = False
 
-    torch_lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                               num_layers=num_layers, bias=bias, dropout=inter_layer_dropout,
-                               batch_first=True, bidirectional=False)
+        self.compare_custom_and_cuda(batch_size, length, input_size, hidden_size, num_layers, bias,
+                                     inter_layer_dropout, recurrent_dropout, batch_first, skip_connection,
+                                     jit_forward_custom)
 
-    custom_lstm = lstm.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, bias=bias,
-                            inter_layer_dropout=inter_layer_dropout, recurrent_dropout=recurrent_dropout,
-                            skip_connection=skip_connection)
+    def test_lstm_basic_single_layer_jit(self):
+        self.set_seed()
+        # :param input: batch size x length x input_size
+        # :param hx: num_layers x batch_size x hidden_size
 
-    self.copy_weights_from_torch_to_custom(torch_lstm, custom_lstm)
+        batch_size = 13
+        length = 4
+        input_size = 32
+        hidden_size = 7
+        num_layers = 1
+        bias = True
+        inter_layer_dropout = 0.
+        recurrent_dropout = 0.
+        batch_first = True
+        skip_connection = False
+        jit_forward_custom = True
 
-    torch_output, torch_hidden = torch_lstm(input, hx)
-    custom_output, custom_hidden = custom_lstm(input, hx)
+        self.compare_custom_and_cuda(batch_size, length, input_size, hidden_size, num_layers, bias,
+                                     inter_layer_dropout, recurrent_dropout, batch_first, skip_connection,
+                                     jit_forward_custom)
 
-    torch_testing.assert_allclose(torch_hidden[0], custom_hidden[0])
-    torch_testing.assert_allclose(torch_hidden[1], custom_hidden[1])
+    @staticmethod
+    def compare_custom_and_cuda(batch_size, length, input_size, hidden_size, num_layers, bias,
+                                inter_layer_dropout, recurrent_dropout, batch_first, skip_connection,
+                                jit_forward_custom):
+        if batch_first:
+            input = torch.rand(batch_size, length, input_size)
+        else:
+            input = torch.rand(length, batch_size, input_size)
 
-    torch_testing.assert_allclose(custom_output, torch_output)
+        hx = torch.rand(num_layers, batch_size, hidden_size), torch.rand(num_layers, batch_size, hidden_size)
 
-  def test_lstm_basic_multi_layer_without_bias(self):
-    self.set_seed()
-    # :param input: batch size x length x input_size
-    # :param hx: num_layers x batch_size x hidden_size
+        torch_lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                                   num_layers=num_layers, bias=bias, dropout=inter_layer_dropout,
+                                   batch_first=batch_first, bidirectional=False)
 
-    batch_size = 13
-    length = 7
-    input_size = 32
-    hidden_size = 64
-    num_layers = 4
-    bias = False
-    inter_layer_dropout = 0.
-    recurrent_dropout = 0.
-    skip_connection = False
+        custom_lstm = lstm.LSTM(input_size=input_size, hidden_size=hidden_size,
+                                num_layers=num_layers, bias=bias,
+                                inter_layer_dropout=inter_layer_dropout, recurrent_dropout=recurrent_dropout,
+                                skip_connection=skip_connection, batch_first=batch_first)
 
-    input = torch.rand(batch_size, length, input_size)
-    hx = torch.rand(num_layers, batch_size, hidden_size), torch.rand(num_layers, batch_size, hidden_size)
+        CustomLSTMTest.copy_weights_from_torch_to_custom(torch_lstm, custom_lstm, bias)
 
-    torch_lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                               num_layers=num_layers, bias=bias, dropout=inter_layer_dropout,
-                               batch_first=True, bidirectional=False)
+        if jit_forward_custom:
+            custom_lstm_forward = torch.jit.script(custom_lstm)
+        else:
+            custom_lstm_forward = custom_lstm
+        torch_output, torch_hidden = torch_lstm(input, hx)
+        custom_output, custom_hidden = custom_lstm_forward(input, hx)
 
-    custom_lstm = lstm.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, bias=bias,
-                            inter_layer_dropout=inter_layer_dropout, recurrent_dropout=recurrent_dropout,
-                            skip_connection=skip_connection)
+        torch_output.sum().backward()
+        custom_output.sum().backward()
 
-    self.copy_weights_from_torch_to_custom(torch_lstm, custom_lstm, bias=bias)
+        torch_testing.assert_allclose(torch_hidden[0], custom_hidden[0])
+        torch_testing.assert_allclose(torch_hidden[1], custom_hidden[1])
 
-    torch_output, torch_hidden = torch_lstm(input, hx)
-    custom_output, custom_hidden = custom_lstm(input, hx)
+        torch_testing.assert_allclose(custom_output, torch_output)
 
-    torch_testing.assert_allclose(torch_hidden[0], custom_hidden[0])
-    torch_testing.assert_allclose(torch_hidden[1], custom_hidden[1])
+        torch_grads = [p.grad for p in torch_lstm.parameters() if p.grad is not None]
+        custom_grads = [p.grad for p in custom_lstm.parameters() if p.grad is not None]
 
-    torch_testing.assert_allclose(custom_output, torch_output)
+        assert len(torch_grads) == len(custom_grads)
 
-  def test_lstm_basic_single_layer_jit(self):
-    self.set_seed()
-    # :param input: batch size x length x input_size
-    # :param hx: num_layers x batch_size x hidden_size
+        for i in range(len(torch_grads)):
+            torch_testing.assert_allclose(torch_grads[i], custom_grads[i])
 
-    batch_size = 13
-    length = 4
-    input_size = 32
-    hidden_size = 64
-    num_layers = 1
-    bias = True
-    inter_layer_dropout = 0.
-    recurrent_dropout = 0.
-    input = torch.rand(batch_size, length, input_size)
-    hx = torch.rand(num_layers, batch_size, hidden_size), torch.rand(num_layers, batch_size, hidden_size)
+    @staticmethod
+    def copy_weights_from_torch_to_custom(torch_model, custom_model, bias=True):
+        c = 0
+        parameter_list = list(torch_model.parameters())
+        for i in range(len(custom_model.layers)):
+            custom_model.layers[i].weight_ih = parameter_list[c]
+            custom_model.layers[i].weight_hh = parameter_list[c + 1]
+            if bias:
+                custom_model.layers[i].bias_ih = parameter_list[c + 2]
+                custom_model.layers[i].bias_hh = parameter_list[c + 3]
+            c += 4 if bias else 2
 
-    torch_lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                               num_layers=num_layers, bias=bias, dropout=inter_layer_dropout,
-                               batch_first=True, bidirectional=False)
-
-    custom_lstm = lstm.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, bias=bias,
-                            inter_layer_dropout=inter_layer_dropout, recurrent_dropout=recurrent_dropout)
-
-    self.copy_weights_from_torch_to_custom(torch_lstm, custom_lstm)
-
-    custom_lstm = torch.jit.script(custom_lstm)
-
-    torch_output, torch_hidden = torch_lstm(input, hx)
-    custom_output, custom_hidden = custom_lstm(input, hx)
-
-    torch_testing.assert_allclose(torch_hidden[0], custom_hidden[0])
-    torch_testing.assert_allclose(torch_hidden[1], custom_hidden[1])
-
-    torch_testing.assert_allclose(custom_output, torch_output)
-
-  @staticmethod
-  def copy_weights_from_torch_to_custom(torch_model, custom_model, bias=True):
-    c = 0
-    parameter_list = list(torch_model.parameters())
-    for i in range(len(custom_model.layers)):
-      custom_model.layers[i].weight_ih = parameter_list[c]
-      custom_model.layers[i].weight_hh = parameter_list[c + 1]
-      if bias:
-        custom_model.layers[i].bias_ih = parameter_list[c + 2]
-        custom_model.layers[i].bias_hh = parameter_list[c + 3]
-      c += 4 if bias else 2
-
-  @staticmethod
-  def set_seed(seed=0):
-    torch.random.manual_seed(seed)
-    # random.seed(seed)
-    # np.random.seed(seed)
+    @staticmethod
+    def set_seed(seed=0):
+        torch.random.manual_seed(seed)
+        # random.seed(seed)
+        # np.random.seed(seed)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
