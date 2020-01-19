aa45b1a61efeee56245764b596b75a9a2c0b0c98
Arvan <mo.arvan@gmail.com>
2020-01-19 21:55:01 +0000

    add sequence_dropout

---
+++ word_language_model/sequence_dropout.py
@@ -0,0 +1,37 @@
+import torch.nn
+
+
+class SequenceDropout(torch.nn.Dropout):
+    """[summary]
+    
+    Args:
+        torch ([type]): [description]
+    
+    Returns:
+        [type]: [description]
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(SequenceDropout, self).__init__(*args, **kwargs)
+        self.mask: torch.Tensor = torch.empty(1)
+        self.new_mask = False
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """[summary]
+
+        Args:
+            x (torch.Tensor): [description]
+
+        Returns:
+            torch.Tensor: [description]
+        """
+        if not self.training:
+            return x
+        if self.new_mask:
+            self.mask = torch.zeros_like(x).bernoulli_(p=1 - self.p)
+            self.new_mask = False
+
+        return x * self.mask
+
+    def generate_new_mask(self):
+        self.new_mask = True
