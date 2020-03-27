8a49ee01098f8fd86f28bdacf5b352d95bb37bde
Arvan <mo.arvan@gmail.com>
2020-03-26 20:57:26 -0500

    Minor comment

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -222,7 +222,7 @@ eval_batch_size = 10
 test_batch_size = 1
 
 train_data = batchify(corpus.train, args.batch_size)
-train_eval_data = batchify(corpus.train, eval_batch_size)
+# train_eval_data = batchify(corpus.train, eval_batch_size)
 val_data = batchify(corpus.valid, eval_batch_size)
 test_data = batchify(corpus.test, test_batch_size)
 
